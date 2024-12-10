897dc4e23925c01cf9189dfdc23c76a5aad55124
Jonathan Rodríguez <jlc.rodriguez316@gmail.com>
2024-12-10 11:24:17 -0500

    empezando login

--- index.php
+++ index.php
@@ -663,3 +663,49 @@
     
 </body>
 </html>
+
+
+<!-- Button trigger modal -->
+<button width="80px" height="130px" style="position: absolute; left: 4%; top: 15%"; type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">INICIO DE SESION</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+         <div class="col-md-12">
+            <div class="form-group">
+              <label for="">USUARIO/EMAIL</label>
+              <input type="email" class="form-control" style="border: 1px solid #ccc;
+                                                              margin-bottom: 15px;
+                                                              box-shadow: 9px 3px 12px #000;
+                                                              margin-top: 15px;">
+           </div>
+         </div>
+         <div class="col-md-12">
+            <div class="form-group">
+              <label for="">CONTRASEÑA</label>
+              <input type="password" class="form-control" style="border: 1px solid #ccc;
+                                                              margin-bottom: 15px;
+                                                              box-shadow: 9px 3px 12px #000;
+                                                              margin-top: 15px;">
+           </div>
+         </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
