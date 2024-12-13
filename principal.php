8552ed2e88d61630e2e2c6d223e81c2cd9dc0fea
Jonathan Rodríguez <jlc.rodriguez316@gmail.com>
2024-12-13 17:28:22 -0500

    Incorporando templeate adminLTE

--- principal.php
+++ principal.php
@@ -4,15 +4,34 @@ include('app/config.php');
 
 session_start();
 if(isset($_SESSION['usuario_sesion'])){
-    echo "existe sesion";
+    //echo "existe sesion";
     ?>
-    <a href="<?php echo $URL;?>/login/cerrar_sesion.php">CERRAR SESSION</a>
+    
     <?php
 }else{
-    echo "Para ingresar necesita iniciar session";
+    //echo "Para ingresar necesita iniciar session";
 }
 
-
 //echo "Bienvenido Admininstrador";
-
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <?php include('layout/admin/head.php');?>
+</head>
+<body class="hold-transition sidebar-mini">
+
+<div class="wrapper">
+<?php include('layout/admin/menu.php');?>
+
+  <div class="content-wrapper">
+    BIENVENIDO ADMINISTRADOR
+
+
+    </div>
+    <?php include('layout/admin/footer.php');?>
+</div>
+<?php include('layout/admin/footer_link.php');?>
+</body>
+</html>
---
+++ usuarios/index.php
@@ -0,0 +1,21 @@
+<?php include('../app/config.php'); ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <?php include('../layout/admin/head.php');?>
+</head>
+<body class="hold-transition sidebar-mini">
+
+<div class="wrapper">
+<?php include('../layout/admin/menu.php');?>
+
+  <div class="content-wrapper">
+    BIENVENIDO ADMINISTRADOR
+
+
+    </div>
+    <?php include('../layout/admin/footer.php');?>
+</div>
+<?php include('../layout/admin/footer_link.php');?>
+</body>
+</html>
